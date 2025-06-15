846754be51adf1b47f4e4bbac3acb4c765ffc7a5
Abir <abirmoulick998@gmail.com>
2025-06-15 08:04:50 +0530

    q

--- .config/dmenu_scripts/cliphistd.sh
+++ .config/dmenu_scripts/cliphistd.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
+# last=""
+# cliphist_path="$HOME/.config/dmenu_scripts/cliphist.sh"
+
+# while true; do
+#   clip="$(xsel -o -b)"
+#   if [ -n "$clip" ]; then
+#     printf "%s" "$clip" | "$cliphist_path" out
+#     last="$clip"
+#   fi
+#   sleep 1
+# done
+
+#!/usr/bin/env bash
+
 last=""
 cliphist_path="$HOME/.config/dmenu_scripts/cliphist.sh"
 
-while true; do
+while clipnotify; do
   clip="$(xsel -o -b)"
-  if [ -n "$clip" ]; then
+  if [ -n "$clip" ] && [ "$clip" != "$last" ]; then
     printf "%s" "$clip" | "$cliphist_path" out
     last="$clip"
   fi
-  sleep 1
 done
-
