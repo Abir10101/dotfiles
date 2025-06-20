d713c0fae7b47f1fa99ade8224ca259b3d845bb7
abirmoulick <abirmoulick998@gmail.com>
2025-06-20 16:33:07 +0530

    q

--- .config/dmenu_scripts/powermenu.sh
+++ .config/dmenu_scripts/powermenu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 case "$(printf "zzz\nreboot\nshutdown" | dmenu -i -l 3 -p 'Power menu:')" in
-	zzz) slock systemctl suspend -i ;;
-	reboot) systemctl reboot -i ;;
-	shutdown) shutdown now ;;
+	zzz) slock && sudo zzz ;;
+	reboot) sudo reboot ;;
+	shutdown) sudo poweroff ;;
 	*) exit 1 ;;
 esac
